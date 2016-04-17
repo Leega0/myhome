6e1f2ea4aa17a917dc1d8da62e252ff0abfe8b26
leegao <igeekeer@qq.com>
2016-04-17 22:25:15 +0800

    享元模式 例tooltip

--- node/svg.js
+++ node/svg.js
@@ -1,58 +1,47 @@
-var CalendarItem = new Interface('CalendarItem',['display']);
-
-var CalendarYear = function(year,parent) {
-	this.year = year;
+var ToolTip = function(targetElement,text){
+	this.target = targetElement;
+	this.text = text;
+	this.delayTimeout = null;
+	this.delaytime = 1500;
+	// 开始创建dom
 	this.element = document.createElement('div');
 	this.element.style.display = 'none';
-	parent.appendChild(this.element);
-	function isLeapYear(y){
-		return (y>0)&&!(y%4)&&((y%100)||!(y%400))
-	}
-	this.months=[];
-	this.numDays = [31,isLeapYear(this.year)?29:28,31,30,31,30,31,31,30,31,30,31];
-	for(var i =0;len=12;i<len;i++){
-		this.months[i]=new CalendarMonth(i,this.numDays[i],this.element);
-	}
-}
-CalendarYear.prototype = {
-	display:function(){
-		for(var i = 0;len = this.months.length;i<len;i++){
-			this.months[i].display();
+	this.element.style.position = 'absolute';
+	this.element.className = 'tooltip';
+	document.getElementsByTagName('body')[0].appendChild(this.element);
+	this.element.innerHTML = this.text;
+
+	// 绑定事件
+	var that = this;
+	this.target.addEventListener('mouseover',function(e){
+		that.showDelay(e);
+	});
+	this.target.addEventListener('mouseout',function(e){
+		that.hide();
+	});
+};
+ToolTip.prototype = {
+	showDelay:function(e){
+		if (this.delayTimeout==null) {
+			var that =this;
+			var x = e.clientX;
+			var y = e.clientY;
+			this.delayTimeout = setTimeout(function(){
+				that.show(x,y);
+			},this.delaytime);
 		}
+	},
+	show:function(x,y){
+		clearTimeout(this.delayTimeout);
+		this.delayTimeout = null;
+		this.element.style.left = x+'px';
+		this.element.style.top = (y+20)+'px';
 		this.element.style.display = 'block';
+	},
+	hide:function(){
+		clearTimeout(this.delayTimeout);
+		this.delayTimeout = null;
+		this.element.style.display = 'none';
 	}
 };
-var CalendarMonth = function(monthNum,numDays,parent){
-	this.monthNum = monthNum;
-	this.element = document.createElement('div');
-	this.element.style.display = 'none';
-	parent.appendChild(this.element);
-
-	this.days = [];
-	for (var i = 0; len = numDays;i < len; i++) {
-		this.days[i] = calendarDay
-	}
-};
-CalendarMonth.prototype ={
-	display:function(){
-		for(var i = 0;len = this.days.length;i<len;i++){
-			this.days[i].display(i,this.element)
-		}
-		this.element.style.display ='block';
-	}
-}
-var CalendarDay = function(){}
-CalendarDay.prototype = {
-	display:function(date,parent){
-		var element = document.createElement('div');
-		parent.appendChild(element);
-		element.innerHTML = date;
-	}
-};
-var calendarDay = new CalendarDay();
-
-
-
-
-
-
+var tt = new ToolTip(linkElement,'hello')
