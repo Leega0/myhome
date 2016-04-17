2fcdbe8c05cfbe82f5bef619fc9cbdee69187ef0
leegao <igeekeer@qq.com>
2016-04-17 13:55:46 +0800

    享元模式 例Calendar

--- node/svg.js
+++ node/svg.js
@@ -1,22 +1,58 @@
-function updateTime() {
-	var now = new Date();
-	var seconds = now.getSeconds();
-	var min = (now.getMinutes()%60)+seconds/60;
-	var hour = (now.getHours()%12)+min/60;
-	
+var CalendarItem = new Interface('CalendarItem',['display']);
 
-	var secondangle = seconds*6;
-	var minangle = min*6;
-	var hourangle = hour*30;
-
-	var seconds = document.getElementById('minsecond')
-	var minhand = document.getElementById('minutehand');
-	var hourhand = document.getElementById('hourhand');
-
-	seconds.setAttribute("transform","rotate("+secondangle+",50,50)");
-	minhand.setAttribute("transform","rotate("+minangle+",50,50)");
-	hourhand.setAttribute("transform","rotate("+hourangle+",50,50)");
-	
-
-	setTimeout(updateTime,1000);
+var CalendarYear = function(year,parent) {
+	this.year = year;
+	this.element = document.createElement('div');
+	this.element.style.display = 'none';
+	parent.appendChild(this.element);
+	function isLeapYear(y){
+		return (y>0)&&!(y%4)&&((y%100)||!(y%400))
+	}
+	this.months=[];
+	this.numDays = [31,isLeapYear(this.year)?29:28,31,30,31,30,31,31,30,31,30,31];
+	for(var i =0;len=12;i<len;i++){
+		this.months[i]=new CalendarMonth(i,this.numDays[i],this.element);
+	}
 }
+CalendarYear.prototype = {
+	display:function(){
+		for(var i = 0;len = this.months.length;i<len;i++){
+			this.months[i].display();
+		}
+		this.element.style.display = 'block';
+	}
+};
+var CalendarMonth = function(monthNum,numDays,parent){
+	this.monthNum = monthNum;
+	this.element = document.createElement('div');
+	this.element.style.display = 'none';
+	parent.appendChild(this.element);
+
+	this.days = [];
+	for (var i = 0; len = numDays;i < len; i++) {
+		this.days[i] = calendarDay
+	}
+};
+CalendarMonth.prototype ={
+	display:function(){
+		for(var i = 0;len = this.days.length;i<len;i++){
+			this.days[i].display(i,this.element)
+		}
+		this.element.style.display ='block';
+	}
+}
+var CalendarDay = function(){}
+CalendarDay.prototype = {
+	display:function(date,parent){
+		var element = document.createElement('div');
+		parent.appendChild(element);
+		element.innerHTML = date;
+	}
+};
+var calendarDay = new CalendarDay();
+
+
+
+
+
+
